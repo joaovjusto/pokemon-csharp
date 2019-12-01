e801b05bf8daa6a519cf4b165ef2a998150c429f
João Justo <joaovjusto30@hotmail.com>
2019-12-01 17:38:39 -0300

    Primeiro commit do projeto

---
+++ pokemon-app/pokemon-app/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using pokemon_app.Models;
+
+namespace pokemon_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            //We will make a GET request to a really cool website...
+
+            string baseUrl = "https://pokeapi.co/api/v2/pokemon/ditto/";
+            //The 'using' will help to prevent memory leaks.
+            //Create a new instance of HttpClient
+            using (HttpClient client = new HttpClient())
+
+            //Setting up the response...         
+
+            using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+            using (HttpContent content = res.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    var pokemon = new Pokemon();
+                    pokemon = JsonConvert.
+                    DeserializeObject<Pokemon>(data);
+                    ViewData["pokemon"] = pokemon;
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ pokemon-app/pokemon-app/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pokemon_app.Models
+{
+    public class Pokemon
+    {
+        public String name { get; set; }
+        public String img_url { get; set; }
+        public String user { get; set; }
+        public Boolean time { get; set; }
+        public Int32 habilidade { get; set; }
+    }
+}
